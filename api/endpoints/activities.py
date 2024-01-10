3773e7128037473e6d99ec1e8b0f897f8fd64328
pickgun4life <ericding@yeah.net>
2024-01-10 22:24:16 +0800

    add more activity api

--- api/endpoints/activities.py
+++ api/endpoints/activities.py
@@ -1,4 +1,5 @@
 import binascii
+from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
@@ -13,7 +14,7 @@ router = APIRouter()
 
 
 # 创建活动
-@router.post("/activities/{trip_id}", response_model=schemas.ActivityResponse)
+@router.post("/{trip_id}", response_model=schemas.ActivityResponse)
 def create_activity(
         *,
         db: Session = Depends(deps.get_db),
@@ -48,9 +49,85 @@ def create_activity(
         raise HTTPException(status_code=403, detail="无权限创建活动")
 
     # 创建活动
-    activity = crud.activity.create_by_trip_and_user(db, activity_in=activity_in, creator_id=current_user.id, trip_id=trip_id)
+    activity = crud.activity.create_by_trip_and_user(db, activity_in=activity_in, creator_id=current_user.id,
+                                                     trip_id=trip_id)
 
     # 将数据库中的坐标由WKBELement转为WKT
+    activity.coordinates = to_shape(activity.coordinates).wkt
+
+    return activity
+
+
+# 更新活动
+@router.put("/{activity_id}", response_model=schemas.ActivityResponse)
+def update_activity(
+        *,
+        db: Session = Depends(deps.get_db),
+        activity_id: int,
+        activity_in: schemas.ActivityUpdate,
+        current_user: models.User = Depends(deps.get_current_user),
+):
+    # 查询活动
+    activity = crud.activity.get(db, id=activity_id)
+    if not activity:
+        raise HTTPException(status_code=404, detail="活动不存在")
+
+    activity = crud.activity.update(db, db_activity=activity, activity_in=activity_in)
+
+    # 返回之前，将数据库中的坐标由WKBELement转为WKT，否则无法序列化
+    activity.coordinates = to_shape(activity.coordinates).wkt
+
+    return activity
+
+
+# 根据 id 查询活动
+@router.get("/{activity_id}", response_model=schemas.ActivityResponse)
+def read_activity(
+        *,
+        db: Session = Depends(deps.get_db),
+        activity_id: int,
+        current_user: models.User = Depends(deps.get_current_user),
+):
+    activity = crud.activity.get(db, id=activity_id)
+    if not activity:
+        raise HTTPException(status_code=404, detail="活动不存在")
+
+    # 返回之前，将数据库中的坐标由WKBELement转为WKT，否则无法序列化
+    activity.coordinates = to_shape(activity.coordinates).wkt
+
+    return activity
+
+
+# 查询所有活动
+@router.get("/", response_model=List[schemas.ActivityResponse])
+def read_activities(
+        db: Session = Depends(deps.get_db),
+        current_user: models.User = Depends(deps.get_current_user),
+):
+    activities = crud.activity.get_multi(db)
+
+    # 返回之前，将数据库中的坐标由WKBELement转为WKT，否则无法序列化
+    for activity in activities:
+        activity.coordinates = to_shape(activity.coordinates).wkt
+
+    return activities
+
+
+# 删除活动
+@router.delete("/{activity_id}", response_model=schemas.ActivityResponse)
+def delete_activity(
+        *,
+        db: Session = Depends(deps.get_db),
+        activity_id: int,
+        current_user: models.User = Depends(deps.get_current_user),
+):
+    activity = crud.activity.get(db, id=activity_id)
+    if not activity:
+        raise HTTPException(status_code=404, detail="活动不存在")
+
+    activity = crud.activity.remove(db, id=activity_id)
+
+    # 返回之前，将数据库中的坐标由WKBELement转为WKT，否则无法序列化
     activity.coordinates = to_shape(activity.coordinates).wkt
 
     return activity
--- crud/activity.py
+++ crud/activity.py
@@ -12,13 +12,35 @@ class CRUDActivity(CRUDBase[Activity, ActivityCreate, ActivityUpdate]):
     def create_by_trip_and_user(self, db: Session, *, activity_in: ActivityCreate, creator_id: int,
                                 trip_id: int) -> Activity:
         coordinates_wkt = f'POINT({activity_in.coordinates.longitude} {activity_in.coordinates.latitude})' if activity_in.coordinates else None
-        # if activity_in.coordinates:
-        #     coordinates_wkt = Point(activity_in.coordinates.longitude, activity_in.coordinates.latitude)
-        # else:
-        #     coordinates_wkt = None
         activity_in_data = jsonable_encoder(activity_in)
         activity_in_data['coordinates'] = coordinates_wkt
         db_activity = Activity(**activity_in_data, creator_id=creator_id, trip_id=trip_id)
+        db.add(db_activity)
+        db.commit()
+        db.refresh(db_activity)
+        return db_activity
+
+    # 更新重写，主要是为了转换地理坐标格式
+    def update(self, db: Session, *, db_activity: Activity, activity_in: ActivityUpdate) -> Activity:
+        # 将坐标数据暂存下来
+        coordinates_wkt = db_activity.coordinates
+        db_activity.coordinates = None  # 不这么操作就不能对 db_activity 进行jsonable_encoder 操作
+        activity_data = jsonable_encoder(db_activity)
+        if isinstance(activity_in, dict):
+            update_activity = activity_in
+        else:
+            update_activity = activity_in.dict(exclude_unset=True)
+
+        if update_activity.get('coordinates'):
+            update_activity[
+                'coordinates'] = f'POINT({activity_in.coordinates.longitude} {activity_in.coordinates.latitude})'
+        else:
+            update_activity['coordinates'] = coordinates_wkt
+
+        for field in activity_data:
+            if field in update_activity:
+                setattr(db_activity, field, update_activity[field])
+
         db.add(db_activity)
         db.commit()
         db.refresh(db_activity)
